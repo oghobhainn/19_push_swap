1481951cfdc77b2a69067375378ad3613129b629
Charlie Mcgahan <cmcgahan@mi-r3-p1.s19.be>
2021-03-05 17:20:45 +0100

    cleaning repo

--- c_compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc checker/*.c shared/*.c -o checker_program
-chmod +x checker_program
--- ps_compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc push_swap/*.c shared/*.c -o push_swap_program
-chmod +x push_swap_program
